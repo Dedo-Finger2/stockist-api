c5215221b667f5f238efd18f1e4fb9946eba5364
Greg <antonioimportant@gmail.com>
2024-05-14 22:38:57 -0300

    feat: add ProductTypeEntity

---
+++ src/domain/entities/ProductType.ts
@@ -0,0 +1,28 @@
+import { Entity } from "@/core/domain/Entity";
+
+interface IProductTypeProps {
+  name: string;
+  description?: string;
+  userId: string;
+  createdAt?: Date;
+}
+
+export class ProductType extends Entity<IProductTypeProps> {
+  constructor(props: IProductTypeProps, id?: string) {
+    super(props, id);
+  }
+
+  public static create(
+    { name, description, userId, createdAt }: IProductTypeProps,
+    id?: string,
+  ): ProductType {
+    const props: IProductTypeProps = {
+      name,
+      description,
+      userId,
+      createdAt: createdAt ?? new Date(),
+    };
+    const productType = new ProductType(props, id);
+    return productType;
+  }
+}
